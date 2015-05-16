b091aa7229a72c052848c01bb9f126852c3bc62f
zouhair <zouhair@zee.aero>
2015-05-15 18:54:11 -0700

    working version of MCMC with toy data

--- MCMC/toy.py
+++ MCMC/toy.py
@@ -19,32 +19,37 @@ def seeP(P_array):
     return np.column_stack([Pleft, p])
 
 
-x_true = np.array([0., 0., -10., 10.])
-y_true = np.array([0., 10., 14., 7.])
+x_true = np.array([-5., 0., -10., 10., 4.])
+y_true = np.array([0., 10., 14., 7., -5.])
 
-Nsamples = 100
-s_o = np.random.random_integers(0,1, Nsamples)
+P_true = np.array( [[0., .4, .2]])
 
-s_d = np.array(s_o)
-for i in range(2):
-    idx = s_o == i
-    d = 1;
-    if i == 1:
-      d = 3
-    s_d[idx] =  np.random.random_integers(i+1, d, np.sum(idx))    
-    
-#s_d = np.random.random_integers(0,3, Nsamples)
+Nsamples_data = 400
+
+# s_o = np.random.random_integers(0,1, Nsamples_data)
+# 
+# s_d = np.array(s_o)
+# for i in range(2):
+#     idx = s_o == i
+#     d = 1;
+#     if i == 1:
+#       d = 3
+#     s_d[idx] =  np.random.random_integers(i+1, d, np.sum(idx))    
+
+s_o = np.random.random_integers(0, len(x_true)-1, Nsamples_data)    
+s_d = np.random.random_integers(0, 1, Nsamples_data)
 
 x_orig = x_true[s_o]; y_orig = y_true[s_o]
 x_dest = x_true[s_d]; y_dest = y_true[s_d]
 
-fracs_true = np.random.random_sample(Nsamples) 
-x_meas = (x_dest - x_orig) * fracs_true + x_orig
-y_meas = (y_dest - y_orig) * fracs_true + y_orig
+fracs_true = np.random.random_sample(Nsamples_data) 
+x_meas = (x_dest - x_orig) * fracs_true + x_orig + np.random.randn(Nsamples_data)*0.25
+y_meas = (y_dest - y_orig) * fracs_true + y_orig + np.random.randn(Nsamples_data)*0.25
 xy_meas = np.column_stack([x_meas, y_meas]);
 
 
-Nnodes= 4
+Nnodes= 5
+Nsamples = 300
 #state_origin = pymc.DiscreteUniform('origin',    lower=0, upper=Nnodes-1, size=Nsamples)
 state_origin = np.array(range(Nnodes)) #np.random.randint(low=0, high=Nnodes, size=Nsamples)
 
@@ -67,9 +72,9 @@ y_nodes = np.array([pymc.Uniform('y_nodes_%i'%i, lower=-15., upper=15.) for i in
 #     return out
 
 
-Prows =  np.empty(4, dtype=object)
+Prows =  np.empty(Nnodes, dtype=object)
 for i in range(Nnodes):
-    t = np.ones(4)*10; t[i] = 0.1
+    t = np.ones(Nnodes)*10; t[i] = 0.1
     Prows[i] = pymc.Dirichlet('Dir_%i'%i, theta=t)
 
 @pymc.deterministic
@@ -81,28 +86,33 @@ def P_s_tp1(P=Prows, st=state_origin):
   
   
 Nsamples_multi = Nsamples/Nnodes
-s_tp1 = np.array([pymc.Multinomial('multi_%i'%i, p=P_s_tp1[i], n=Nsamples_multi, plot=False) for i in range(Nnodes)])
-
+# s_tp1 = np.array([pymc.Multinomial('multi_%i'%i, p=P_s_tp1[i], n=Nsamples_multi, plot=False) for i in range(Nnodes)])
 frac = np.random.rand(Nsamples_multi)
 
 @pymc.deterministic
-def xy_points(s_o=state_origin, s_t1=s_tp1, f=frac,
+def xy_points(s_o=state_origin, f=frac,
               x_n=x_nodes, y_n=y_nodes, 
               Prows=Prows): 
     #Note that we manually define the Dirichlet as a parent (Prows)
     #Somehow it doesn't recognize that!
-    s_tlist = np.array(range(Nnodes))
     
     xy_m = np.empty((Nsamples_multi*Nnodes, 2))
     popidx = range(Nsamples_multi);
+    
     for s_origin in s_o:
         x_o = x_n[s_origin]; 
         y_o = y_n[s_origin];
         
-        #s_d = np.array([np.dot(s_tlist, v) for v in s_t1[s_origin]])
-        s_d = [ [s for i in range(v)] for (s,v) in enumerate(s_t1[s_origin])]
-        #turn into numpy array
-        s_d = np.array([s for ll in s_d for s in ll])
+#         s_d = [ [s for i in range(v)] for (s,v) in enumerate(s_t1[s_origin])]
+#         #turn into numpy array
+#         s_d = np.array([s for ll in s_d for s in ll])
+
+        counts = (Prows[s_origin] * Nsamples_multi).astype(int)
+        
+        #counts = np.round(np.round(Prows[s_origin], 3) * Nsamples_multi).astype(int)        
+        counts = np.append(counts, Nsamples_multi - counts.sum())
+        
+        s_d = np.array([s for (s, c) in enumerate(counts) for i in range(c)]) 
         
         #grab destination nodes
         x_d = x_n[s_d]
