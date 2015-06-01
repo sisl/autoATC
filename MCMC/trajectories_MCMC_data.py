2d6ca8e74f74c8647a3b92a612e90f47674545e0
zouhair <zouhair@zee.aero>
2015-06-01 10:11:30 -0700

    commited new file

---
+++ MCMC/trajectories_MCMC_data.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+import scipy.io
+data = scipy.io.loadmat("../4D_points_sim_v7.3.mat");
+Xfull = data['XBig']
+
+data = scipy.io.loadmat("../turningpoints_means_sim_v7.3.mat");
+Xnodes = data['Xnodes']
+
+#xy_meas = Xfull[0:3000, 0:2]
+
+idx = np.arange(Xfull.shape[0])
+np.random.shuffle(idx)
+xy_meas = Xfull[idx[0:3000], 0:2]
