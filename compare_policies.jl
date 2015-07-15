7e2761575c645227a22e2bf4c4d6b2a515d515d2
zouhair <zouhair.mahboubi@gmail.com>
2015-07-14 18:36:23 -0700

    slight different in counting actions

--- compare_policies.jl
+++ compare_policies.jl
@@ -11,6 +11,28 @@ function printStats(gammas, cnt, N)
     println("-------------------")
 end
 
+
+
+function sameAction(S::pattern.SType, p1::pattern.extActType, p2::pattern.extActType)
+    if p1 == p2
+        return true
+    end
+    
+    ac1  = p1[1]; ac2  = p2[1];
+    des1 = p1[2]; des2 = p2[2];
+    
+    if ac1 != 0 && ac2 != 0
+        #Doesn't matter, we are just talking to different aircraft
+        if S[ac1] == S[ac2] && des1 == des2
+            return true
+        end
+    end
+    
+    return false
+end
+
+
+
 gammas = Float64[0.5, 0.7, 1.0]
 cnt = Dict{Float64, Int64}()
 for g in gammas cnt[g] = 0 end
@@ -29,13 +51,13 @@ for cidx in 1:CTMDP_kronsolver.g_nScomp #rand(1:CTMDP_kronsolver.g_nScomp, N)
     for γ in gammas
         mcts.pars.γ = γ
         p1 = mctsPolicy(S);
-        if p1 != p2
+        if !sameAction(S, p1, p2)
             #try again just in case since this is MCTS!
             p1 = mctsPolicy(S);
-                if p1 != p2 && p1[1] != 0 && !(string(S[p1[1]]) in ["LArr", "RArr"])
+            if !sameAction(S, p1, p2)
                 cnt[γ] += 1
                 #println(S, "-> M:", p1, " | C", p2)
-                end
+            end
         end
     end
     now = time()
